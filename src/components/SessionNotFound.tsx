44c7fa522eda483fdc6c8379140b6d0f995da6c8
Kai Black <kaibblack03+github@gmail.com>
2025-02-23 03:32:11 -0800

    Updated SessionNotFound component for custom message

--- src/components/SessionNotFound.tsx
+++ src/components/SessionNotFound.tsx
@@ -1,9 +1,13 @@
 import SignIn from './SignIn';
 
-const SessionNotFound = () => {
+interface SessionNotFoundProps {
+    message: string;
+}
+
+const SessionNotFound = ({ message }: SessionNotFoundProps) => {
     return (
         <div className="container mx-auto px-4 py-8">
-            <h1 className="text-2xl font-bold mb-8">Please Sign In to create or see your degree plans</h1>
+            <h1 className="text-2xl font-bold mb-8">Please Sign In {message}</h1>
             <SignIn />
         </div>
     );
